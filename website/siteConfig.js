49de9c24aae3826bb1e0c6c6299b7ef5b6af0818
naomiaro <naomiaro@gmail.com>
2020-09-21 22:21:35 +0100

    add download top

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -38,6 +38,7 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
+    { page: "#try", label: "Download" },
     { page: "stories", label: "Stories" },
     { doc: "intro", label: "Docs" },
     { page: "help", label: "Help" },
