5bcaa03d905022d6fcdf5576bfe881776ff03f85
Naomi Aro <naomiaro@gmail.com>
2022-02-13 11:05:58 -0800

    run build

--- website/.docusaurus/client-modules.js
+++ website/.docusaurus/client-modules.js
@@ -1,4 +1,5 @@
 export default [
+  require("/Users/naomiaro/Code/mural-docusaurus/website/node_modules/@docusaurus/plugin-google-analytics/lib/analytics"),
   require("/Users/naomiaro/Code/mural-docusaurus/website/node_modules/infima/dist/css/default/default.css"),
   require("/Users/naomiaro/Code/mural-docusaurus/website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
   require("/Users/naomiaro/Code/mural-docusaurus/website/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
--- website/.docusaurus/routes.js
+++ website/.docusaurus/routes.js
@@ -4,41 +4,6 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
-    path: '/__docusaurus/debug/',
-    component: ComponentCreator('/__docusaurus/debug/','60e'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/config/',
-    component: ComponentCreator('/__docusaurus/debug/config/','647'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/content/',
-    component: ComponentCreator('/__docusaurus/debug/content/','8e2'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/globalData/',
-    component: ComponentCreator('/__docusaurus/debug/globalData/','4f6'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/metadata/',
-    component: ComponentCreator('/__docusaurus/debug/metadata/','363'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/registry/',
-    component: ComponentCreator('/__docusaurus/debug/registry/','fb4'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/routes/',
-    component: ComponentCreator('/__docusaurus/debug/routes/','b09'),
-    exact: true
-  },
-  {
     path: '/blog/',
     component: ComponentCreator('/blog/','0a6'),
     exact: true
